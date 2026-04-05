2fd0548f22a00fdae216413ac0c3474fcef9ad89
Роман Васютенко <vasyutenko20050205@mail.ru>
2026-04-05 22:27:18 +0300

    finish

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -1,20 +1,5 @@
-import { ActionIcon, Affix } from "@mantine/core";
-import { SunIcon } from "@phosphor-icons/react";
 import { Outlet } from "react-router-dom";
 
 export const Root = () => {
-
-  return (
-    <>
-      {/* <Affix position={{ bottom: 20, right: 20 }}>
-        <ActionIcon
-          size={"xl"}
-          onClick={() => {}}
-        >
-          <SunIcon />
-        </ActionIcon>
-      </Affix> */}
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
